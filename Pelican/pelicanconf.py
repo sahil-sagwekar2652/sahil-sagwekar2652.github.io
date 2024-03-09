3d4b4c1bbbd3e1238aab58ec8797977b92376f51
sahil-sagwekar2652 <sahilss2652@gmail.com>
2024-03-09 20:13:29 +0530

    update theme MinimalXY

--- Pelican/pelicanconf.py
+++ Pelican/pelicanconf.py
@@ -22,7 +22,6 @@ LINKS = (
     ("Pelican", "https://getpelican.com/"),
     ("Python.org", "https://www.python.org/"),
     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
 )
 
 DEFAULT_PAGINATION = False
@@ -48,14 +47,14 @@ DEFAULT_PAGINATION = False
 THEME = "/home/sahil/Sahil/blog/Pelican/pelican-themes/MinimalXY"
 
 # Theme customizations
-# MINIMALXY_CUSTOM_CSS = 'static/custom.css'
-# MINIMALXY_FAVICON = 'favicon.ico'
+# MINIMALXY_CUSTOM_CSS = "static/custom.css"
+# MINIMALXY_FAVICON = "favicon.ico"
 MINIMALXY_START_YEAR = 2023
 MINIMALXY_CURRENT_YEAR = datetime.date.today().year
 
 # Author
 AUTHOR_INTRO = "Hello world! I’m Sahil Sagwekar."
-AUTHOR_DESCRIPTION = "Hello world! I’m John Doe. Developer and FOSS enthusiast."
+AUTHOR_DESCRIPTION = "Hello world! I’m Sahil Sagwekar. Developer and FOSS enthusiast."
 AUTHOR_AVATAR = "https://www.gravatar.com/avatar/14d6aae5677b0a937900e452ab55d29effa852d31e3fbf971e20a89112441559?s=240"
 AUTHOR_WEB = "https://sahil-sagwekar2652.github.io"
 
